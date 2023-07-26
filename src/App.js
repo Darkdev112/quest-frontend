3857fb4222b97942459d2f0dfc4b288c238251a9
Jahnavi Lenka <jahnavi2003lenka@gmail.com>
2023-07-26 14:50:49 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Landing from './pages/Landing'
 import Domain from './pages/Domain'
 import Login from './component/Login'
 import Sign_up from './component/SignUp'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div>
-      <Landing/>
+      <Dashboard/>
     </div>
   )
 }
