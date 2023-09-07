aea3b2da6184518cdb242103a0cd1c9e9a1e0124
Jahnavi Lenka <jahnavi2003lenka@gmail.com>
2023-09-07 22:29:38 +0530

    questions added

---
+++ src/component/Questions/Bulimia.jsx
@@ -0,0 +1,55 @@
+[
+    {
+       question : "How often are you engaging in bingeing and purging behaviors?",
+       addiction : "Bulimia",
+       options :["Always", "Often", "Sometimes","Rarely","Never"]
+   },
+   {
+    
+     question : "What are some of the thoughts and feelings that lead to episodes of bingeing and purging?",
+     addiction : "Bulimia",
+     options : ["Depression","Anxiety","Insecurity about body shape","Low self-esteem","Genetic and social ideas"]
+ },
+ {
+    
+   question :  "How is your experience with bulimia?:",
+   addiction : "Bulimia",
+   options : ["Very badly","Badly", "Moderately","Balanced","None,I am cured"]
+ },
+{
+ question : "Do you feel in control during these episodes of bulimia, or does it feel more impulsive?:",
+ addiction : "Bulimia",
+ options :["Always", "Often", "Sometimes","Rarely","Never"]
+},
+{
+    
+ question : "Are you experiencing any physical symptoms as a result of your behaviors?:",
+ addiction : "Bulimia",
+ options : ["Swollen cheeks ,fainting","Dehydration","Bloodshot eyes and Gastrointestinal issues","Some symptoms but not regular","None,I am cured"]
+},
+{   
+ question : "Has bulimia affected your daily life, relationships, and overall well-being?",
+ addiction : "Bulimia",
+ options : ["Always", "Often", "Sometimes","Rarely","Not anymore"]
+},
+{   
+ question : "Have you ever tried to stop these behaviors on your own?",
+ addiction : "Bulimia",
+ options :  ["Never","Rarely","Sometimes", "Often","Always"]
+},
+{   
+ question : "Do you have a support system in place, such as friends, family, or other loved ones?",
+ addiction : "Bulimia",
+ options :  ["Never","Rarely","Sometimes", "Often","Always"]
+},
+{   
+ question : "How do you feel about the idea of working towards a healthier relationship with food and your body?",
+ addiction : "Bulimia",
+ options :  ["Do not appreciate","Bad","Moderate", "Good" ,"Very good"]
+},
+{   
+    question : "Would you be open to exploring coping strategies that don't involve bingeing and purging?",
+    addiction : "Bulimia",
+    options :  ["Never","Rarely","Sometimes", "Often","Always"]
+   }, 
+];
---
+++ src/component/Questions/Social_media.jsx
@@ -0,0 +1,60 @@
+[
+    {
+       question : "How has your excessive use of social media affected your daily life and relationships?",
+       addiction : "Social Media",
+       options :["Badly" , "Highly" ,"Medially", "Less" ,"Not anymore"]
+   },
+   {
+    
+     question : "How many hours a day ,do you usually invest in social media?",
+     addiction : "Social Media",
+     options : ["More than 12 hours","8-10 hours","4-6 hours","2-3 hours" ,"Less than 2 hours"]
+ },
+ {
+    
+   question :  "Can you describe any specific negative consequences or situations that have arisen due to your social media usage?",
+   addiction : "Social Media",
+   options : ["Depression and Anxiety","Cyberbullying and FOMO(Fear of missing out)","Unrealistic expectations and general addiction","Unrealistic sleep addiction","None"]
+ },
+{
+ question : "What are the situations or emotions that typically lead you to use social media excessively?",
+ addiction : "Social Media",
+ options :["Low self-esteem", "Personal Dissatisfaction","Depression and hyperactivity","Peer pressure" ,"None"]
+},
+{
+    
+ question : "What negative consequences have you experienced as a result of your gambling?",
+ addiction : "Social Media",
+ options : ["Financial Problems","Legal Issues","Stained Relations.", "Bad influence" , "None"]
+},
+{   
+ question : "Do you find it hard to control your social media addiction?",
+ addiction : "Social Media",
+ options : ["Always", "Often", "Sometimes","Rarely","Not anymore"]
+},
+{   
+ question : "How does using social media make you feel?",
+ addiction : "Social Media",
+ options :  ["Fulfilling","Fulfilling","Moderate","Draining","Unaffected"]
+},
+{   
+ question : "What do you think you're seeking or trying to fulfill through your use of social media?",
+ addiction : "Social Media",
+ options :  ["Gain attention", "Acting on peer pressure","Building self-esteem","Feeling connected","Nothing,just using"]
+},
+{   
+ question : "Do you thing social media is affecting your life?",
+ addiction : "Social Media",
+ options : ["Always", "Often", "Sometimes","Rarely","Not anymore"]
+},
+{   
+    question : "Would you be open to using tools or techniques that help you monitor and control your social media usage?",
+    addiction : "Social Media",
+    options :  ["Never","Rarely","Sometimes", "Often","Always"]
+   }, 
+   {
+    question: "What steps can you take to prevent relapse and maintain a healthier relationship with social media in the long run?",
+    addiction : "Social Media",
+    options: [ "None","Not interested","I will try","Take precautions" ,"Work on improving everyday"],
+},
+];
